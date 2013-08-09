8db6523d289b6dfed2fcb0e7561e05624e5807a7
Mike Lowe <j.michael.lowe@gmail.com>
2013-08-09 11:29:19 -0400

    changed from complicated regex for parsing to something simpler added Resource_List.gres as a field changed from log timestamp to end for completion time dropped start records

--- pbspump.py
+++ pbspump.py
@@ -1,4 +1,4 @@
-#! /bin/env python2.7
+#! /usr/bin/python
 import simpledaemon, logging
 from datetime import datetime, timedelta
 from dateutil import parser as dateparser
@@ -9,14 +9,16 @@ from time import sleep,mktime
 from kombu.connection import BrokerConnection
 from kombu.messaging import Producer,Exchange
 
-entire_history = 'yes'
+entire_history = 'no'
 
-logpat = re.compile('(.{19});E;(\d+)(?:-(\d+))?\..*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+) .* exec_host=(\S+) (?:Resource_List.mem=(\S+))?.* Resource_List.walltime=(\d+:\d+:\d+) .*Exit_status=(\S+) .*resources_used.mem=(\d+).*\n(\S+)')
-jobstartpat = re.compile('(.{19});S;(\d+)(?:-(\d+))?\..*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+).*exec_host=(\S+).*(?:Resource_List.mem=(\S+))?.*Resource_List.walltime=(\d+:\d+:\d+).*\n(\S+)')
+logpat = re.compile('(.{19});E;(\d+)(?:-(\d+))?.*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+) .* exec_host=(\S+).*(?:Resource_List.gres=(\S+))?.*(?:Resource_List.mem=(\S+))?.*Resource_List.walltime=(\d+:\d+:\d+).*Exit_status=(\S+).*resources_used.mem=(\d+).*\n(\S+)')
+jobstartpat = re.compile('(.{19});S;(\d+)(?:-(\d+))?\..*;user=(\S+) (?:account=(\S+))?.*group=(\S+).*queue=(\S+) ctime=\d+ qtime=(\d+) etime=(\d+) start=(\d+).*exec_host=(\S+).*(?:Resource_List.gres=(\S+))?.*(?:Resource_List.mem=(\S+))?.*Resource_List.walltime=(\d+:\d+:\d+).*\n(\S+)')
 
 exechostpat = re.compile('/\d+')
 
-colnames = ('type','original_log_line','completion_time','jobid','step','username','project','group','queue','submit_time','eligibletime','start_time','nodelist','requested_mem','walltime','exit_status','mem','filename')
+colnames = ('type','original_log_line','completion_time','jobid','step','username','project','group','queue','submit_time','eligibletime','start_time','nodelist','gres','requested_mem','walltime','exit_status','mem','filename')
+
+conversiondict = {'nodelist':'exec_host' ,'username':'user', 'group':'group','completion_time':'end','project':'account','queue':'queue','submit_time':'qtime','eligibletime':'etime','start_time':'start','gres':'Resource_List.gres','requested_mem':'Resource_List.mem','walltime':'Resource_List.walltime','exit_status':'Exit_status','mem':'resources_used.mem',}
 
 def uniquify(seq, idfun=None): 
     # order preserving
@@ -38,21 +40,26 @@ def stepconvert(step):
   if not step:
     return 0
   return int(step)
-def requested_mem_convert(requested_mem):
-  if requested_mem == None:
-    returnvalue = 0 
-  elif requested_mem.endswith('gb'):
-    returnvalue = int(requested_mem.replace('gb',''))*1024*1024*1024
-  elif requested_mem.endswith('mb'):
-    returnvalue = int(requested_mem.replace('mb',''))*1024*1024
-  elif requested_mem.endswith('kb'):
-    returnvalue = int(requested_mem.replace('kb',''))*1024
-  elif requested_mem.endswith('b'):
-    returnvalue = int(requested_mem.replace('b',''))
+def mem_convert(mem):
+  if mem == None:
+      returnvalue = 0
+  elif mem.endswith('gb'):
+    returnvalue = int(mem.replace('gb',''))*1024*1024*1024
+  elif mem.endswith('mb'):
+    returnvalue = int(mem.replace('mb',''))*1024*1024
+  elif mem.endswith('kb'):
+    returnvalue = int(mem.replace('kb',''))*1024
+  elif mem.endswith('b'):
+    returnvalue = int(mem.replace('b',''))
+  elif mem.isdigit():
+    returnvalue = int(mem)
   else:
     returnvalue = 0
   return returnvalue
+
 def walltimeconvert(walltime):
+  if not walltime:
+    return 0
   hours,minutes,seconds = walltime.split(':')
   return (int(hours)*60+int(minutes))*60+int(seconds)
 #  return timedelta(0,(int(hours)*60+int(minutes))*60+int(seconds),0)
@@ -77,6 +84,7 @@ def gen_cat():
   logging.info('Now reading from %s' % filename)
   while True:
     for item in s.readlines():
+      #logging.debug(item)
       yield item + s.name.replace('/var/spool/torque/server_priv/accounting/','')
     sleep(60)
     if os.stat(filename).st_size == s.tell() and not today ==  '%d%02d%02d' % (datetime.now().year,datetime.now().month,datetime.now().day):
@@ -97,7 +105,7 @@ def gen_open(filenames):
       yield open(name)
 
 def field_map(dictseq,name,func, dep_name= None):
-  logging.debug(`[dictseq,name,func,dep_name]`)
+#  logging.debug(`[dictseq,name,func,dep_name]`)
   if dep_name == None:
     dep_name = name
   for d in dictseq:
@@ -108,6 +116,7 @@ def groups_gen(lines):
   for line in lines:
     exitmatch = logpat.match(line)
     if exitmatch:
+#       logging.debug(line)
        yield (('exit',line.strip()))+exitmatch.groups()
     elif jobstartpat.match(line):
        g = (('start',line.strip()))+jobstartpat.match(line).groups()
@@ -119,16 +128,45 @@ def job_start_completion_map(dictseq):
       d['completion_time']+=d['walltime']
     yield d
 
+def jobdict(lines):
+   for line in lines:
+     if ';E;' in line:
+       type = 'exit'
+     elif ';S;' in line:
+       type = 'start'
+       continue
+     else:
+       continue
+     splitline = line.split()
+#     logging.debug(`splitline`)
+     linedict = dict((x.split('=') for x in splitline[2:-1]+splitline[1].split(';')[3:] if not 'ppn=' in x))
+     jobdict = {'original_line':line, 'type':type, 'filename':splitline[-1]}
+     jobid = re.match('.*;E;(\d+)(?:-(\d+))?.*;user=\S+',splitline[1]).groups()
+     jobdict['jobid'] = jobid[0]
+     if len(jobid) > 1:
+       jobdict['step'] =  jobid[1]
+     else:
+       jobdict['step'] = None
+     for item in conversiondict:
+       if conversiondict[item] in linedict:
+         jobdict[item] = linedict[conversiondict[item]]
+       else:
+         jobdict[item] = None
+     yield jobdict
+
+
 def jobs():
   lines = gen_cat()
 #  groups = (logpat.match(line) for line in lines)
 #  tuples = (g.groups() for g in groups if g)
-  tuples = groups_gen(lines)
-  log = (dict(zip(colnames,t)) for t in tuples)
-  log = field_map(log,"completion_time",lambda x: int(dateparser.parse(x).strftime('%s')))
+#  tuples = groups_gen(lines)
+#  log = (dict(zip(colnames,t)) for t in tuples)
+  log = jobdict(lines)
+#  log = field_map(log,"completion_time",lambda x: int(dateparser.parse(x).strftime('%s')))
 #  log = field_map(log,"submit_time",lambda x: datetime.fromtimestamp(int(x)))
 #  log = field_map(log,"start_time",lambda x: datetime.fromtimestamp(int(x)))
 #  log = field_map(log,"eligibletime", lambda x: datetime.fromtimestamp(int(x)))
+  log = field_map(log,"completion_time", int)
   log = field_map(log,"start_time", int)
   log = field_map(log,"eligibletime", int)
   log = field_map(log,"submit_time", int)
@@ -143,9 +181,9 @@ def jobs():
   log = field_map(log,"group",unicode)
   log = field_map(log,"queue",unicode)
   log = field_map(log,"exit_status",exit_status_convert)
-  log = field_map(log,"mem",int)
+  log = field_map(log,"mem",mem_convert)
   log = field_map(log,"filename",unicode)
-  log = field_map(log,"requested_mem",requested_mem_convert)
+  log = field_map(log,"requested_mem",mem_convert)
   return log
 
 class PBSPumpDaemon(simpledaemon.Daemon):
